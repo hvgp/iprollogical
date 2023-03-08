55b67a4795bbf9ad8310c6f3dfe8fd1efbdf9984
hvgp <h.porter23@icloud.com>
2023-03-08 15:31:19 +1100

    Adds notes and examples of operator definition and usage

--- 01-Anaxandridas.pl
+++ 01-Anaxandridas.pl
@@ -39,3 +39,24 @@ heir(Successor, Predecessor) :- father(Predecessor, Successor), archagetai(Prede
 % List; structure. Consequent / head variable: universally quantified. Antecedent variable: existentially quantified.
 lineage([Solitary]) :- archagetai(Solitary).
 lineage([Reigning, Scion | Progeny]) :- heir(Scion, Reigning), lineage([Scion | Progeny]). % Direct / indirect recursion.
+
+% Operator; notation. Definition: predicate, directive. Precedence, associativity, functor.
+% Infix; binary predicate: xfx, xfy, yfx. Prefix; unary: fx, fy. Postfix: unary: xf, yf.
+% Prefix; unary: fx, fy.
+succeeded(Successor, Predecessor) :- archagetai(Successor), heir(Successor, Predecessor).
+:- op(200, yfx, succeeded).
+
+/*
+    =  , /=  : Args unify.
+    == , /== : Args identical.
+    =:=, =/= : Evaluate identical nos. Also < > =< >=
+    R is E   : E is no. R unbound, bound to E. Bound no, succeeds as per =:=
+*/
+
+% Redefinition of lineage predicate using `succeeded` operator.
+succession([Final]) :- archagetai(Final).
+succession([Predecessor, Successor | Descendants]) :-
+    Successor succeeded Predecessor,
+    succession([Successor | Descendants]).
+
+:- op(200, fy, succession). % Prefix operator. `succession X`.
